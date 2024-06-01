80c867e0b021ecf9af3123fdb992aeb2d61b9fcd
Johan Kjölhede <johan.kjolhede@gmail.com>
2024-06-01 10:37:05 +0200

    add some more tests

--- cmd/testmain_compose/testmain_test.go
+++ cmd/testmain_compose/testmain_test.go
@@ -1,7 +1,28 @@
 package main
 
-import "testing"
+import (
+	"github.com/GiGurra/boa/cmd/test_common"
+	"testing"
+)
 
 func TestRunMain(t *testing.T) {
-	main()
+	test_common.RunTests(t, main, []test_common.TestSpec{
+		{
+			Name:     "no args",
+			Args:     []string{},
+			Expected: "missing required param 'foo'",
+		},
+		{
+			Name: "with args",
+			Args: []string{
+				"--foo", "foo", "--bar", "1", "--file", "file",
+				"--foo2", "foo", "--bar2", "1", "--file2", "file",
+				"--foo3", "foo", "--bar3", "1", "--file3", "file",
+				"--foo24", "foo", "--bar24", "1", "--file24", "file",
+				"--baz", "baz",
+			},
+			Expected: "Hello world from subcommand1 with params: foo, 1, file, baz, <nil>, <nil>",
+		},
+	})
+
 }
