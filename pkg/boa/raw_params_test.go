5a164ad30ca93a3b06c5d4c1a078a55b250e8c9f
Johan Kjölhede <gigurra@gmail.com>
2025-05-01 15:11:52 +0200

    wip on raw values

--- pkg/boa/raw_params_test.go
+++ pkg/boa/raw_params_test.go
@@ -1,6 +1,7 @@
 package boa
 
 import (
+	"github.com/spf13/cobra"
 	"strconv"
 	"testing"
 )
@@ -10,7 +11,9 @@ type RawConfig struct {
 	Port   int              `long:"port" env:"PORT" default:"8080"`
 	Extra1 string           `long:"extra1" env:"EXTRA1" required:"false"`
 	Extra2 string           `long:"extra2" env:"EXTRA2" optional:"true"`
-	Extra3 string           `long:"extra2" env:"EXTRA2" required:"true" default:"blah"`
+	Extra3 string           `long:"extra3" env:"EXTRA3" required:"true" default:"blah"`
+	Extra4 string           `long:"extra4" env:"EXTRA4" required:"true"`
+	Extra5 string           `long:"extra5" env:"EXTRA5" required:"true"`
 }
 
 func TestRawConfig(t *testing.T) {
@@ -20,17 +23,24 @@ func TestRawConfig(t *testing.T) {
 		Port:   12345,
 		Extra1: "ex1",
 		Extra2: "ex2",
-		Extra3: "blah",
+		Extra3: "blah",          // default is used
+		Extra4: "from-file",     // config file value is used
+		Extra5: "not-from-file", // config file value is overridden by cli arg
 	}
 
 	config := RawConfig{}
 
 	err := NewCmdT2("root", &config).
+		WithPreValidateFunc(func(params *RawConfig, cmd *cobra.Command, args []string) {
+			params.Extra4 = "from-file"
+			params.Extra5 = "from-file"
+		}).
 		WithRawArgs([]string{
 			"--host", expected.Host.Value(),
 			"--port", strconv.Itoa(expected.Port),
 			"--extra1", expected.Extra1,
 			"--extra2", expected.Extra2,
+			"--extra5", "not-from-file",
 		}).
 		Validate()
 
@@ -56,5 +66,13 @@ func TestRawConfig(t *testing.T) {
 
 	if config.Extra3 != expected.Extra3 {
 		t.Fatalf("Expected Extra3: %v, got: %v", expected.Extra3, config.Extra3)
+	}
+
+	if config.Extra4 != expected.Extra4 {
+		t.Fatalf("Expected Extra4: %v, got: %v", expected.Extra4, config.Extra4)
+	}
+
+	if config.Extra5 != expected.Extra5 {
+		t.Fatalf("Expected Extra5: %v, got: %v", expected.Extra5, config.Extra5)
 	}
 }
